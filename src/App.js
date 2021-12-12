3626e10d8e2b865165ec9375b4d885c647f3ee7d
tommy solano <tommysolano18@hotmail.com>
2021-12-12 03:50:29 -0500

    filtro de visivilidad

--- src/App.js
+++ src/App.js
@@ -1,39 +1,96 @@
+import {useDispatch, useSelector} from "react-redux"
 import { useState } from "react"
-import { useDispatch, useSelector } from "react-redux"
 
-export const reducer = (state = 0, action) => {
-  switch (action.type) {
-    case "incrementar":
-      return state + 1
-    case "decrementar":
-      return state - 1
-    case "set":
-      return action.payload
-    default:
-      return state
-  }
+const initialState = {
+    entities: [],
+    filter: "all", // complete || incomplete
 }
 
-function App() {
-  const [valor, setValor] = useState("")
-  const dispatch = useDispatch()
-  const state = useSelector(state => state)
-  const set = () => {
-    dispatch({type: "set", payload: valor})
-    setValor("")
-  }
-  return (
-    <div>
-      <p>Contador: {state}</p>
-      <button onClick={() => dispatch({type: "incrementar"})}> Incrementar </button>
-      <button onClick={() => dispatch({type: "decrementar"})} >Decrementar</button>
-      <button onClick={set} >Set</button>
-      <input value={valor} onChange={e => setValor(Number(e.target.value))} />
-    </div>
-  );
+export const reducer = (state = initialState, action ) => {
+    switch (action.type) {
+        case "todo/add": {
+            return {
+                ...state,
+                entities: state.entities.concat({ ...action.payload })
+            }
+        }
+        case "todo/complete": {
+            const newTodos = state.entities.map(todo => {
+                if (todo.id === action.payload.id) {
+                    return { ...todo, completed: !todo.completed}
+                }
+                return todo
+            })
+            return {
+                ...state,
+                entities: newTodos  
+            }
+        }
+        case "filter/set": {
+            return {
+                ...state,
+                filter: action.payload
+            }
+        }
+        default:
+            return state
+    }
 }
 
-// los valores que se encuentran dentro de los campos de los formularios no debemos guardarlos en redux
-//useDispatch sirve para hacer el dispatch de las acciones
-// para usar los datos que se encuentran dentro del reducer usamos useSelector
-export default App;
+const selectTodos = state => {
+    const { entities, filter } = state
+
+    if ( filter === "complete") {
+        return entities.filter(todo => todo.completed)
+    }
+
+    if ( filter === "incomplete") {
+        return entities.filter(todo => !todo.completed)
+    }
+
+    return entities
+}
+
+const TodoItem = ({todo}) => {
+    const dispatch = useDispatch()
+
+    return (
+        <li
+            style={{ textDecoration: todo.completed ? "line-through" : "none"}}
+            onClick={() => dispatch({ type: "todo/complete", payload: todo})}
+        >{todo.title}</li>
+    )
+}
+
+const App = () => {
+    const [value, setValue] = useState("")
+    const dispatch = useDispatch()
+    const todos = useSelector(selectTodos)
+   
+    const submit = e => {
+        e.preventDefault()
+        if (!value.trim()){
+            return 
+        }
+        const id = Math.random().toString(36)
+        const todo = { title: value, completed: false, id}
+        dispatch( {type: "todo/add", payload: todo} )
+        setValue("")
+    }
+
+    return (
+        <div>
+            <form onSubmit={submit}>
+                <input value={value} onChange={e => setValue(e.target.value)} />
+            </form>
+            <button onClick={() => dispatch({ type: "filter/set", payload: "all"})} >Mostrar todos</button>
+            <button onClick={() => dispatch({ type: "filter/set", payload: "complete"})} >Completados</button>
+            <button onClick={() => dispatch({ type: "filter/set", payload: "incomplete"})} >Imcompletos</button>
+            <ul>
+                {todos.map(todo => <TodoItem key={todo.id} todo={todo}/>)}
+            </ul>
+        </div>
+    )
+}
+
+export default App
